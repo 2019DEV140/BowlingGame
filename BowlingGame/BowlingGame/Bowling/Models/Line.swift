641667f0e13da828d498333d8f9028d54b2ac70d
Unknown <olikity@hotmail.be>
2019-03-16 18:04:04 +0100

    Line creation

--- BowlingGame/BowlingGame/Bowling/Models/Line.swift
+++ BowlingGame/BowlingGame/Bowling/Models/Line.swift
@@ -13,6 +13,29 @@ class Line {
     }
     
     func createFrames(withRolls rolls: [Int]) -> [Frame] {
+        var frames = [Frame]()
+        var previousRoll: Int? = nil;
         
+        for roll in rolls {
+            if roll == 10 {
+                let frame = Frame.init(tries: [roll])
+                frames.append(frame)
+            } else {
+                if previousRoll == nil {
+                    previousRoll = roll
+                } else {
+                    let frame = Frame.init(tries: [previousRoll!, roll])
+                    frames.append(frame)
+                    previousRoll = nil
+                }
+            }
+        }
+        
+        if previousRoll != nil {
+            let frame = Frame.init(tries: [previousRoll!])
+            frames.append(frame)
+        }
+        
+        return frames
     }
 }
