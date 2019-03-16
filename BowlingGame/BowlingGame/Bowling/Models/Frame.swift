87d55a4274513a121fe06932e0fee89c7ee2ca81
Unknown <olikity@hotmail.be>
2019-03-16 17:07:14 +0100

    typeOfFrame

--- BowlingGame/BowlingGame/Bowling/Models/Frame.swift
+++ BowlingGame/BowlingGame/Bowling/Models/Frame.swift
@@ -5,6 +5,25 @@
 
 import Foundation
 
+enum TypeFrame {
+    case strike
+    case spare
+    case notAllKnocked
+}
+
 struct Frame {
     var tries: [Int]
+    
+    func type() -> TypeFrame {
+        if tries.count == 1 {
+            return .strike
+        } else {
+            let sum = tries[0] + tries[1]
+            if sum == 10 {
+                return .spare
+            } else {
+                return .notAllKnocked
+            }
+        }
+    }
 }
