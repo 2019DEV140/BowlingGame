867d42a622875eb9eda0d16eb8d7633ef5a45624
Unknown <olikity@hotmail.be>
2019-03-17 14:01:22 +0100

    Some UI

--- BowlingGame/BowlingGame/ViewController.swift
+++ BowlingGame/BowlingGame/ViewController.swift
@@ -6,12 +6,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: -
+    // MARK: IBOutlets
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var fullStrikeButton: UIButton!
+    @IBOutlet weak var fullSpareButton: UIButton!
+    @IBOutlet weak var fullNineButton: UIButton!
+    
+    // MARK: -
+    // MARK: Life Cycle
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        fullStrikeButton.round(withRadius: 5)
+        fullSpareButton.round(withRadius: 5)
+        fullNineButton.round(withRadius: 5)
     }
-
-
+    
+    // MARK: -
+    // MARK: IBAction
+    @IBAction func FullStrikeButtonTapped(_ sender: Any) {
+        let line = Line(withRolls: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
+        scoreLabel.text = "\(line.score())"
+    }
+    
+    @IBAction func FullSpareButtonTapped(_ sender: Any) {
+        let line = Line(withRolls: [5, 5,
+                                    5, 5,
+                                    5, 5,
+                                    5, 5,
+                                    5, 5,
+                                    5, 5,
+                                    5, 5,
+                                    5, 5,
+                                    5, 5,
+                                    5, 5, 5])
+        scoreLabel.text = "\(line.score())"
+    }
+    
+    @IBAction func FullNineButtonTapped(_ sender: Any) {
+        let line = Line(withRolls: [9, 0,
+                                    9, 0,
+                                    9, 0,
+                                    9, 0,
+                                    9, 0,
+                                    9, 0,
+                                    9, 0,
+                                    9, 0,
+                                    9, 0,
+                                    9, 0])
+        scoreLabel.text = "\(line.score())"
+    }
 }
 
+extension UIView {
+    func round(withRadius radius: CGFloat) {
+        layer.cornerRadius = radius
+        clipsToBounds = true
+    }
+}
